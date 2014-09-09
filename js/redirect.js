25f6574bd077de0588d4eee5ee6a613fa9b7e3c9
David Lu <dayal29699@gmail.com>
2014-09-09 03:04:14 -0700

    Redirect chinese traffic to cn.calculasian.com
    resolve #9

--- js/redirect.js
+++ js/redirect.js
@@ -1,7 +1,21 @@
-// Detects browser language
+// Detect URL and browser language
 var lang = navigator.language || navigator.userLanguage;
-if (lang.substr(0, 3) == "zh-") {
-	location = "cn.html";
+if (getParameterByName('lang') === 'en') {
+  location = "http://calculasian.com/en.html";
+} else if (location.hostname === 'cn.calculasian.com') {
+  location = "http://cn.calculasian.com/cn.html";
+} else if (lang.substr(0, 3) == "zh-") {
+  location = "http://cn.calculasian.com/cn.html";
+} else if (location.hostname === 'localhost') {
+  location = "en.html";
 } else {
-  location = "en.html"
+  location = "http://calculasian.com/en.html";
+}
+
+// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
