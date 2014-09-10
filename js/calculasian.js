2f347ab666cc986d1a90689a9919e582d2a584e0
David Lu <dayal29699@gmail.com>
2014-09-10 02:37:47 -0700

    calculasian.js uses requirejs to require jquery

--- js/calculasian.js
+++ js/calculasian.js
@@ -1,101 +1,104 @@
-$(function() {
+require(['jquery'], function($) {
+  $(function() {
 
-	var lang = getLanguage(),
-			homeVideoId,
-			urls = [];
-	loadPages(function() {
-		// Finish constructing video pages
-		$('#fsn').on('fsn-ready', function(e) {  // FSN is ready
-			// Load home page video
-			$('#home').on('fsn-current', function(e, $page) {
+  	var lang = getLanguage(),
+  			homeVideoId,
+  			urls = [];
+  	loadPages(function() {
+  		// Finish constructing video pages
+  		$('#fsn').on('fsn-ready', function(e) {  // FSN is ready
+  			// Load home page video
+  			$('#home').on('fsn-current', function(e, $page) {
 
-				// Skip if iframe is already present
-					if ($('#home-page .video').has('iframe').length == 0) {
-						$('#home-page .video').append($('<iframe>', {'src': urls[homeVideoId], 'frameborder': 0}));
-						$('#home-page iframe')[0].onload = function() {
-							$('.loading' + homeVideoId).remove();
-						};
-					}
-			});
+  				// Skip if iframe is already present
+  					if ($('#home-page .video').has('iframe').length == 0) {
+  						$('#home-page .video').append($('<iframe>', {'src': urls[homeVideoId], 'frameborder': 0}));
+  						$('#home-page iframe')[0].onload = function() {
+  							$('.loading' + homeVideoId).remove();
+  						};
+  					}
+  			});
 
-			// Load other videos
-			$('article[id^=video-page]').on('fsn-current', function(e, $page) {
-				// Get index from video page id
-				var i = $(this).attr('id').replace( /^\D+/g, '');
+  			// Load other videos
+  			$('article[id^=video-page]').on('fsn-current', function(e, $page) {
+  				// Get index from video page id
+  				var i = $(this).attr('id').replace( /^\D+/g, '');
 
-				// Skip if iframe is already present
-				$('.video' + i).each(function(index, element) {
-					if ($(element).has('iframe').length == 0) {
-						$iframe = $('<iframe>', {'src': urls[i], 'frameborder': 0});
-						$(element).append($iframe);
-					}
+  				// Skip if iframe is already present
+  				$('.video' + i).each(function(index, element) {
+  					if ($(element).has('iframe').length == 0) {
+  						$iframe = $('<iframe>', {'src': urls[i], 'frameborder': 0});
+  						$(element).append($iframe);
+  					}
 
-					$iframe[0].onload = function() {
-						$('.loading' + i).remove();
-					};
-				});
+  					$iframe[0].onload = function() {
+  						$('.loading' + i).remove();
+  					};
+  				});
 
-			});
-		});
-	});
+  			});
+  		});
+  	});
 
-	function loadPages(callback) {
-		$('#fsn').load('templates/' + lang +'.html', null, function() {
-			// Include Templates
-			$('#home').load('templates/home.html', null, function() {
-				loadVideoPages();
-			});
-			$('#members').load('templates/members.html');
-			$('#aboutEN').load('templates/aboutEN.html');
-			$('#aboutCN').load('templates/aboutCN.html');
+  	function loadPages(callback) {
+  		$('#fsn').load('templates/' + lang +'.html', null, function() {
+  			// Include Templates
+  			$('#home').load('templates/home.html', null, function() {
+  				loadVideoPages();
+  			});
+  			$('#members').load('templates/members.html');
+  			$('#aboutEN').load('templates/aboutEN.html');
+  			$('#aboutCN').load('templates/aboutCN.html');
 
-			$('#contactEN').load('templates/contactEN.html', null, function() {
-				$('<div>').appendTo('#contact-page').load('templates/donation.html');
-			});	
-			$('#contactCN').load('templates/contactCN.html', null, function() {
-				$('<div>').appendTo('#contact-page').load('templates/donation.html');
-			});
-			callback();
-		});
-	};
+  			$('#contactEN').load('templates/contactEN.html', null, function() {
+  				$('<div>').appendTo('#contact-page').load('templates/donation.html');
+  			});	
+  			$('#contactCN').load('templates/contactCN.html', null, function() {
+  				$('<div>').appendTo('#contact-page').load('templates/donation.html');
+  			});
+  			callback();
+  		});
+  	};
 
-	function loadVideoPages() {
-		// Construct video pages (without video iframes)
-		// Iterate through video objects
+  	function loadVideoPages() {
+  		// Construct video pages (without video iframes)
+  		// Iterate through video objects
 
-		for (var i=0; i < VIDEOS.length; i++) {
-			var video = VIDEOS[i],
-					name = video.name,
-					bgcolor = video.bgcolor,
-					title,
-					desciption;
-			if (lang === 'cn') {
-				title = video.cn.title;
-				description = video.cn.description;
-				urls[i] = video.cn.url;
-			} else {
-				title = video.en.title;
-				description = video.en.description;
-				urls[i] = video.en.url;
-			}
+  		for (var i=0; i < VIDEOS.length; i++) {
+  			var video = VIDEOS[i],
+  					name = video.name,
+  					bgcolor = video.bgcolor,
+  					title,
+  					desciption;
+  			if (lang === 'cn') {
+  				title = video.cn.title;
+  				description = video.cn.description;
+  				urls[i] = video.cn.url;
+  			} else {
+  				title = video.en.title;
+  				description = video.en.description;
+  				urls[i] = video.en.url;
+  			}
 
-			// constuct DOM
-			var $article = $('<article>', {'data-link': name, 'data-background': bgcolor, 'id': 'video-page' + i});
-			var $videoDiv = $('<div>', {'class': 'video video' + i});  // Use class instead of id because there can be multiple instances of the same video
-			$article.append($('<h1>', {text: title}))
-				.append($('<p>', {text: description}))
-				.append($videoDiv);
-			$videoDiv.append($('<img>', {'class': 'ratio', 'src': 'http://placehold.it/16x9'}));
-			$videoDiv.append($('<div>', {'class': 'loading icon-spin6 animate-spin loading' + i}));
-			$('#videos').append($article);
+  			// constuct DOM
+  			var $article = $('<article>', {'data-link': name, 'data-background': bgcolor, 'id': 'video-page' + i});
+  			var $videoDiv = $('<div>', {'class': 'video video' + i});  // Use class instead of id because there can be multiple instances of the same video
+  			$article.append($('<h1>', {text: title}))
+  				.append($('<p>', {text: description}))
+  				.append($videoDiv);
+  			$videoDiv.append($('<img>', {'class': 'ratio', 'src': 'http://placehold.it/16x9'}));
+  			$videoDiv.append($('<div>', {'class': 'loading icon-spin6 animate-spin loading' + i}));
+  			$('#videos').append($article);
 
-			// Set home video
-			if (video.isHomeVideo) {
-				$('#home-page').append($videoDiv.clone());
-				homeVideoId = i;
-			}
-		}	
-	};
+  			// Set home video
+  			if (video.isHomeVideo) {
+  				$('#home-page').append($videoDiv.clone());
+  				homeVideoId = i;
+  			}
+  		}	
+  	};
+  });
+
 });
 
 // Detect URL and browser language
@@ -231,4 +234,3 @@ var VIDEOS =
     }
   }
 ];
-
